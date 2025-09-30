20ea124952394826f5835fa6a3f0772c5a2608be
Mahes222 <maheswaran6381121636@gmail.com>
2025-09-30 12:02:25 +0530

    fear : added code

--- git/r3-code.py
+++ git/r3-code.py
@@ -1,10 +1,10 @@
 from game_round import game
 try:
-    size = int(input("Enter the size of the Box in: ")) 
-    print("Enter the size of the Box in: ")
-    print("Enter the size of the Box in: ")
+    size = int(input("Enter the size of the Box in here: ")) 
+    print("Enter the size of the Box in here: ")
+    print("Enter the size of the Box in here: ")
     games = game(size)
     games.round()
-    print("Enter the size of the Box in: ")
+    print("Enter the size of the Box in here: ")
 except ValueError:
     print("Enter the valid size : ")
