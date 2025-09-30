97f901937dd22397323b5f0f11faf8eee63d9ee6
Mahes222 <maheswaran6381121636@gmail.com>
2025-09-30 11:59:59 +0530

    "feat : changed the input text"

--- git/r3-code.py
+++ git/r3-code.py
@@ -1,7 +1,10 @@
 from game_round import game
 try:
     size = int(input("Enter the size of the Box in: ")) 
+    print("Enter the size of the Box in: ")
+    print("Enter the size of the Box in: ")
     games = game(size)
     games.round()
+    print("Enter the size of the Box in: ")
 except ValueError:
     print("Enter the valid size : ")
